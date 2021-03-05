51ed4beaeec1a154c4f72d785659ce7ad52ae118
RMhoon <r_mhoon@hotmail.com>
2021-03-04 20:27:34 -0500

    Initialize project

---
+++ CSharp_MethodOverloading/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CSharp_MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine($"{Add(1,4,true)}");
+            Console.WriteLine($"{Add(1, 0, true)}");
+            Console.WriteLine($"{Add(12, 4, true)}");
+
+            Console.WriteLine($"No grammar suffix");
+            Console.WriteLine($"{Add(1, 4, false)}");
+            Console.WriteLine($"{Add(1, 0, false)}");
+            Console.WriteLine($"{Add(12, 4, false)}");
+
+
+
+        }
+
+        public static int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+        public static decimal Add(decimal number1, decimal number2)
+        {
+            return number1 + number2;
+        }
+
+        public static string Add(int number1, int number2, bool addGrammar)
+        {
+            int sum = Add(number1, number2);
+
+            return addGrammar ? (sum == 1 ? $"{sum} dollar" : $"{sum} dollars") : $"{sum}";
+        }
+    }
+}
